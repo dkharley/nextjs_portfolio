648ca056e38b860dd4420c66b8af0ff4b198b73d
David <davidsonkharley@gmail.com>
2022-01-20 20:47:05 -0800

    Updated asset url

--- NextJSPortfolio/portfolio/next.config.js
+++ NextJSPortfolio/portfolio/next.config.js
@@ -2,5 +2,5 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/https://dkharley.github.io/nextjs_portfolio/' : '',
+  assetPrefix: isProd ? 'https://dkharley.github.io/nextjs_portfolio/' : '',
 }
