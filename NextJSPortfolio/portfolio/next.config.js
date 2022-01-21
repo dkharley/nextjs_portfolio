4435e5d1f23a340efdea5ca56825d2e53c0908d7
David <davidsonkharley@gmail.com>
2022-01-20 20:51:46 -0800

    Updated url again

--- NextJSPortfolio/portfolio/next.config.js
+++ NextJSPortfolio/portfolio/next.config.js
@@ -1,6 +1,4 @@
-const isProd = process.env.NODE_ENV === 'production'
-
+const prod = process.env.NODE_ENV === 'production'
 module.exports = {
-  reactStrictMode: true,
-  assetPrefix: isProd ? 'https://dkharley.github.io/nextjs_portfolio/' : '',
+  'process.env.BACKEND_URL': prod ? '/nextjs_portfolio' : ''
 }
