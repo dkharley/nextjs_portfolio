db8dc85066edbf1e84e17bbea42d9ffc8ff181ea
David <davidsonkharley@gmail.com>
2022-01-20 20:42:03 -0800

    Added cdn

--- NextJSPortfolio/portfolio/next.config.js
+++ NextJSPortfolio/portfolio/next.config.js
@@ -1,3 +1,6 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
   reactStrictMode: true,
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/https://dkharley.github.io/nextjs_portfolio/' : '',
 }
